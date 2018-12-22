69768018f421e8e6792440c7ffb65b1ee3a0ea70
hwangseonu <hwangseonu12@naver.com>
2018-12-22 18:19:43 +0900

    feat: Add test endpoint for embedded

--- controller/__init__.py
+++ controller/__init__.py
@@ -4,3 +4,5 @@ from flask import Flask
 def init_controller(main: Flask):
     from controller import iot
     main.register_blueprint(iot.blueprint)
+    from controller import test
+    main.register_blueprint(test.blueprint)
