a232c120bb28183b1d30408fd6ce3e2e8e6b73ba
hwangseonu <hwangseonu12@naver.com>
2018-12-22 21:36:37 +0900

    i hate python

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from controller import init_controller
-
-app = Flask(__name__)
-init_controller(app)
-
-app.run()
--- controller/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-
-def init_controller(main: Flask):
-    from controller import iot
-    main.register_blueprint(iot.blueprint)
-    from controller import test
-    main.register_blueprint(test.blueprint)
