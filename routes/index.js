69e57df022af33c8503b7d8d709b777195e8376c
hwangseonu <hwangseonu12@naver.com>
2018-12-23 05:02:38 +0900

    feat: Add action control

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const Serial = require('serialport');
 const router = express.Router();
 
-router.post('/test', function(req, res, next) {
+const ser = new Serial('/dev/ttyACM0', {
+  baudrate: 9600,
+  parser: Serial.parsers.readline('\n')
+});
+
+router.post("/iot", function (req, res, next) {
+  const {type, power} = req.body;
+  ser.write(`${type} ${power}`, function (err) {
+    if (err) {
+      console.error(err);
+      res.json({success: false});
+    } else {
+      console.log(`${type} ${power}`);
+      res.json({success: true});
+    }
+  });
+});
+
+router.post('/test', function (req, res, next) {
   console.log(req.body.message);
   res.json({success: true});
 });
